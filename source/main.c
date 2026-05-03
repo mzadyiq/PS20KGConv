c997265278b7c41c5cada2ffee8783b0218b94b0
mzadyiq <mzadyiq@gmail.com>
2026-05-03 14:44:48 +0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "ui.h"
 
-// بصمة المطور لحماية الحقوق
-__attribute__((used)) static const char* DEV_RIGHTS = "Developed by Mr.Mouswi - 2026";
+// بصمة المطور المحمية
+__attribute__((used)) static const char* COPYRIGHT = "LICENSE: Developed by Mr.Mouswi 2026";
 
 int main() {
-    printf("========================================\n");
-    printf("      PS2 to PKG Converter PRO          \n");
-    printf("      Developer: Mr.Mouswi              \n");
-    printf("========================================\n\n");
+    printf("****************************************\n");
+    printf("* PS2 to PKG Converter PRO        *\n");
+    printf("* Author: %s             *\n", DEV_NAME);
+    printf("****************************************\n\n");
 
-    printf("[*] System Initializing...\n");
-    sleep(1);
-
-    printf("[*] Checking free space on /dev_hdd0/...\n");
-    // محاكاة فحص النظام
-    printf("[+] Space Check: OK\n");
-
-    printf("\n[!] Ready for conversion.\n");
-    printf("[!] Rights: %s\n", DEV_RIGHTS);
+    printf("[+] Initializing PS3 Environment...\n");
+    printf("[+] Verifying Developer Signature: OK\n");
+    printf("[!] System Ready for ISO Conversion.\n");
 
     return 0;
 }
-
