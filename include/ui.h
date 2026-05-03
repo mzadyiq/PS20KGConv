7a616e78c779a809b150db46d2d823a456bda658
mzadyiq <mzadyiq@gmail.com>
2026-05-03 14:30:29 +0300

    Update ui.h

--- include/ui.h
+++ include/ui.h
@@ -1,10 +1,8 @@
 #ifndef UI_H
 #define UI_H
 
-void init_screen();
-void wait_for_user();
-
-#define DEV_SIGNATURE "Created by Mr.Mouswi"
+// تعريفات واجهة التطبيق
+#define APP_VERSION "1.0.0"
+#define DEV_NAME "Mr.Mouswi"
 
 #endif
-
