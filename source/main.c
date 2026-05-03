bc13cc1f37a49dcb8d433e07a9b70a0e95739fb7
mzadyiq <mzadyiq@gmail.com>
2026-05-03 14:49:56 +0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "ui.h"
 
-// بصمة المطور المحمية
+// بصمة المطور لحماية الحقوق (Mr.Mouswi)
 __attribute__((used)) static const char* COPYRIGHT = "LICENSE: Developed by Mr.Mouswi 2026";
 
 int main() {
     printf("****************************************\n");
-    printf("* PS2 to PKG Converter PRO        *\n");
-    printf("* Author: %s             *\n", DEV_NAME);
+    printf("* PS2 to PKG Converter PRO v1.0      *\n");
+    printf("* Dev: %s                *\n", DEV_NAME);
     printf("****************************************\n\n");
 
-    printf("[+] Initializing PS3 Environment...\n");
-    printf("[+] Verifying Developer Signature: OK\n");
-    printf("[!] System Ready for ISO Conversion.\n");
+    printf("[+] Starting System Verification...\n");
+    printf("[+] Core Signature Found: OK\n");
+    printf("[!] Application is Ready for PS2 ISO Conversion.\n");
 
     return 0;
 }
